6e3c13d675d95a5462e7cbeda9e7277242a93b51
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-12-31 17:58:12 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -17,3 +17,15 @@ id INTEGER PRIMARY KEY,
 title TEXT,
 year INTEGER,
 series_id INTEGER);
+
+CREATE TABLE characters(
+id INTEGER PRIMARY KEY,
+name TEXT,
+species TEXT,
+motto TEXT,
+author_id INTEGER);
+
+CREATE TABLE character_books(
+id INTEGER PRIMARY KEY,
+book_id INTEGER,
+character_id INTEGER);
